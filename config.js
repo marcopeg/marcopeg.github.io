50a6b94f81f19a1fb62fce938fbf6ef37e65a5a2
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-02-10 17:40:11 +0100

    basics

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  title: 'Blog by John Doe',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Marco Pegoraro',
+    photo: '/photo.jpg',
+    bio: 'When it comes to write code you don\'t need to learn magic spells. All you need is to take a look outside the window and solve your problem with patterns from the real world, where you come from.',
+    contacts: {
+      email: 'marco.pegoraro@gmail.com',
+      telegram: '',
+      twitter: 'thepeg',
+      github: 'marcopeg',
+      rss: '#',
+      vkontakte: '#'
+    }
+  }
+};
