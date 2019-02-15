651a7e69d82c5e6f9cb0af90c603916962bbb403
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-02-15 06:43:35 +0100

    Article images

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import styles from './Content.module.scss';
+import React from 'react'
+import PostImage from '../../PostImage'
+import styles from './Content.module.scss'
 
 const Content = ({ body, title, image }) => (
-  <div className={styles['content']}>
-    {image && (
-      <img
-        src={`/media/${image}`}
-        alt={title}
-        style={{ margin: '50px auto 0 auto', width: 80, borderRadius: 40 }}
-      />
-    )}
-    <h1 className={styles['content__title']}>{title}</h1>
-    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
-  </div>
-);
+    <div className={styles['content']}>
+        {image && (
+            <div className={styles['content__image']}>
+                <PostImage src={image} />
+            </div>
+        )}
+        <h1 className={styles['content__title']}>{title}</h1>
+        <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+    </div>
+)
 
-export default Content;
+export default Content
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Author from './Author';
-import Comments from './Comments';
-import Content from './Content';
-import Meta from './Meta';
-import Tags from './Tags';
-import styles from './Post.module.scss';
+import React from 'react'
+import { Link } from 'gatsby'
+import Author from './Author'
+import Comments from './Comments'
+import Content from './Content'
+import Meta from './Meta'
+import Tags from './Tags'
+import styles from './Post.module.scss'
 
 const Post = ({ post }) => {
-  const {
-    tags,
-    title,
-    date
-  } = post.frontmatter;
+    const {
+        tags,
+        title,
+        date
+    } = post.frontmatter
 
-  const { html } = post;
-  const { tagSlugs } = post.fields;
+    const { html } = post
+    const { tagSlugs, image } = post.fields
 
-  return (
-    <div className={styles['post']}>
-      <Link className={styles['post__home-button']} to="/">All Articles</Link>
+    return (
+        <div className={styles['post']}>
+            <Link className={styles['post__home-button']} to="/">All Articles</Link>
 
-      <div className={styles['post__content']}>
-        <Content body={html} title={title} image={post.fields.image} />
-      </div>
+            <div className={styles['post__content']}>
+                <Content body={html} title={title} image={image} />
+            </div>
 
-      <div className={styles['post__footer']}>
-        <Meta date={date} />
-        <Tags tags={tags} tagSlugs={tagSlugs} />
-        <Author />
-      </div>
+            <div className={styles['post__footer']}>
+                <Meta date={date} />
+                <Tags tags={tags} tagSlugs={tagSlugs} />
+                <Author />
+            </div>
 
-      <div className={styles['post__comments']}>
-        <Comments postSlug={post.fields.slug} postTitle={post.frontmatter.title} />
-      </div>
-    </div>
-  );
-};
+            <div className={styles['post__comments']}>
+                <Comments postSlug={post.fields.slug} postTitle={post.frontmatter.title} />
+            </div>
+        </div>
+    )
+}
 
-export default Post;
+export default Post
