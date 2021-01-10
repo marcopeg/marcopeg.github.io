071df91c638ea4ea72c3e540c3ecb38da851b402
marcopeg <marco.pegoraro@gmail.com>
2021-01-10 11:51:53 +0000

    Applied customization to the tag cloud

--- src/components/Sidebar/TagCloud/TagCloud.js
+++ src/components/Sidebar/TagCloud/TagCloud.js
@@ -5,17 +5,17 @@ import ReactWordcloud from "react-wordcloud";
 const options = {
   colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
   enableTooltip: false,
-  deterministic: false,
-  fontFamily: "impact",
-  fontSizes: [20, 90],
+  deterministic: true,
+  fontSizes: [15, 60],
+  fontFamily: `apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif`,
   fontStyle: "normal",
   fontWeight: "normal",
   padding: 1,
   rotations: 3,
-  rotationAngles: [0, 90],
+  rotationAngles: [0, 90, -90],
   scale: "sqrt",
   spiral: "archimedean",
-  transitionDuration: 100,
+  transitionDuration: 1000,
 };
 
 const newUrl = (tag) =>{
@@ -27,7 +27,7 @@ const TagListCloud = ({ tagList }) => {
       onWordClick:  newUrl,
     };
 
-   return (<div style={{ height: 400, width: '100%', marginLeft: '-6%'}}>
+   return (<div style={{ height: 300, maxWidth: 300, marginLeft: '-6%'}}>
         <ReactWordcloud options={options} words={tagList} callbacks={callbacks} />
       </div>);
   };
